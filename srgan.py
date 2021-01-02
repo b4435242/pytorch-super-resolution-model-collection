b845ef78f1f7074112e92e802dbdb3e8026737fe
Lin,Bo-Jun <b4435242.cs06@nctu.edu.tw>
2021-01-02 22:19:06 +0800

    all

--- srgan.py
+++ srgan.py
@@ -66,9 +66,9 @@ class Discriminator(torch.nn.Module):
         )
 
         self.dense_layers = nn.Sequential(
-            DenseBlock(base_filter * 8 * image_size // 16 * image_size // 16, base_filter * 16, activation='lrelu',
+            DenseBlock(base_filter * 8, base_filter * 9, activation='lrelu',
                        norm=None),
-            DenseBlock(base_filter * 16, 1, activation='sigmoid', norm=None)
+            DenseBlock(base_filter * 9, 1, activation='sigmoid', norm=None)
         )
 
     def forward(self, x):
